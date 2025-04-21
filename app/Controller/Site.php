187448affc7cfb07f2a622b84c8e39b62b27c9b1
Артём Сергеев <gr431_seade@lab-402-1.tomtit-tomsk.ru>
2025-04-21 11:29:19 +0700

    signup page working

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
 class Site
 {
     public function index(Request $request): string
@@ -16,5 +17,13 @@ class Site
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
     }
 }
