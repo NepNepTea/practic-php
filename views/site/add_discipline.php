ae263977b831b9c017129796386e7ec6637d3696
Артём Сергеев <gr431_seade@lab-402-1.tomtit-tomsk.ru>
2025-04-24 11:49:28 +0700

    add discipline page not complete

---
+++ views/site/add_discipline.php
@@ -0,0 +1,5 @@
+<h1>Добавить дисциплину</h1>
+<form>
+    <label>Название <input type="text" name="name"></label>
+    <button>Создать</button>
+</form>
--- views/site/discipline.php
+++ views/site/discipline.php
@@ -1,4 +1,5 @@
 <h1>Дисциплины</h1>
+<a href="<?= app()->route->getUrl('/add_discipline') ?>">Добавить дисциплину</a>
 <ol>
     <?php
     foreach ($disciplines as $discipline) {
