69f70e5ae8f66fed2f28915fe4c6e478a3788db1
NepNepTea <162781744+NepNepTea@users.noreply.github.com>
2025-04-27 21:21:38 +0700

    Student info page more info

--- views/site/student_info.php
+++ views/site/student_info.php
@@ -1,8 +1,12 @@
-<h1>Студент </h1>
-<ol>
-    <?php
-    foreach ($students as $student) {
-        echo '<li>' . $student->last_name . '</li>';
-    }
-    ?>
-</ol>
+<?php
+foreach ($students as $student) {
+    echo '
+    <h1>Студент ' . $student->last_name . ' ' . $student->first_name . ' ' . $student->patronym . '</h1>
+    <p>Пол: ' . $student->gender . ' </p>
+    <p>Дата рождения: ' . $student->birth_date . ' </p>
+    <p>Адрес прописки: ' . $student->address . ' </p>
+    <p>ID добавившего сотрудника: ' . $student->created_by . ' </p>
+    <p>ID группы: ' . $student->group . ' </p>
+    ';
+}
+?>
